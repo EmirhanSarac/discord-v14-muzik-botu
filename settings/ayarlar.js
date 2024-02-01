803a433a0bce622a2c017c7515a8da522a455284
Baronmarka <158518665+Baronmarka@users.noreply.github.com>
2024-02-02 00:03:12 +0300

    new baron

--- settings/ayarlar.js
+++ settings/ayarlar.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 module.exports = {
-    TOKEN: process.env.TOKEN || "TOKEN GİR",  // bot token gir :)
+    TOKEN: process.env.TOKEN || "MTIwMjcxMTIyMjQyMDc3MDg3Ng.Gzqra-.mfgfgeLufjgOQ9CQcQXti5X79GRGYaH0nQfSWA",  // bot token gir :)
     PREFIX: process.env.PREFIX || "!", // bot prefix
-    SAHİP: process.env.SAHİP || "SAHİP İD", //sahip id :()
+    SAHİP: process.env.SAHİP || "1202711222420770876", //sahip id :()
 }
